97fd2ad5deed77eee29dc8138248c78a9fa4e709
Lalit Patil <147392059+Lalit1702@users.noreply.github.com>
2024-08-22 18:29:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
 function getFormvalue() {
-    //Write your code here
+    // Get the form element
+    const form = document.querySelector('form');
 
+    // Get the values of the First Name and Last Name fields
+    const firstName = form.elements['firstname'].value;
+    const lastName = form.elements['lastname'].value;
+
+    // Display the full name using alert
+    alert(firstName + ' ' + lastName);
 }
